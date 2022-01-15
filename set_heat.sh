653a290980cfc8f3f2daec5d2ad9a3cd43e2e1d6
lucapinello <lucapinello@gmail.com>
2022-01-15 16:08:31 -0500

    settings

--- set_heat.sh
+++ set_heat.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-if  [[ $(cat /dev/shm/FAN) -gt 100 ]] 
+if  [[ $(cat /dev/shm/FAN) -gt 69 ]] 
 
 then 
 	echo $1>dev/shm/HEAT 
+        pigs p 12 $1
 else  
 	echo "OFF" >/dev/shm/HEAT
+        pigs p 12 0
 fi
 
